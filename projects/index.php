d17c255b7ee7e69c52d4fa2553cbd0b78247e0c8
Vito <vito.pramudita1502@gmail.com>
2022-10-18 13:19:56 +0700

    Added new project directory to store project details

---
+++ projects/index.php
@@ -0,0 +1,116 @@
+<!-- it has been months since the last time i touched this website, 
+i hope i could manage my homework well -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="resources/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="resources/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../resources/css/style.css">
+    <link rel="stylesheet" href="../resources/css/responsive.css">
+    <title>vito pm</title>
+</head> 
+<body>
+    <header>
+        <p class="name-logo"><a href="index.php">vitopm</a></p>
+        <div class="nav-bar">
+            <ul>
+                <li><a href="about-me.php">about</a></li>
+                <li><a href="contact.php">contact</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        <div class="intro">
+            <h1 class="intro--name">Vito Minardi</h1>
+            <h3 class="intro--description">Computer science student based in Malang.</h3>
+        </div>
+
+        <div class="content">
+            <p>Welcome to my personal website!&#10024🥳🎉<br>
+                <br>
+                I started to learn making website a while ago.<br>
+                <br>
+                There might be typos here and there or some inconsistency in the design, and that's all the more reason why I have to keep learning.😁<br>
+                <br>
+                I'll keep you guys updated with more projects in the incoming months!<br>
+                <br>
+                Here I attached project(s) I made in the past months sorted from the most recent.<br>
+                <br>
+                Feel free to take a peek! ;)
+            </p>
+
+            <div class="website-list">
+                <a href="https://vitopm-stroke-prediction-app-dsh1w2.streamlitapp.com/" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Stroke Prediction Website</h2>
+                        <p>
+                            A final project for Machine Learning class. Using the K-Nearest Neighbor Algorithm, me and my team developed a stroke prediction app using data from Kaggle. This project is written in python and build on Streamlit framework. The prediction part is built with Sklearn.
+                        </p>
+                    </div>
+                </a>
+
+                <a href="https://vitopm-resume-parser-app-eszpgr.streamlitapp.com/" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Resume Parser Website</h2>
+                        <p>
+                            A final project for Natural Language Processing (NLP) class. Resume parser website is capable to extract information from a CV. The website is written in python and built on Streamlit framework, and the parsing part is built with NLTK and Pyresparser. The project was handled by me, Michelle Angela, and Cheryl Almeira. You can visit the prototype below.
+                        </p>
+                    </div>
+                </a>
+
+                <a href="Resume-Parser" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Resume Parser Website (Prototype)</h2>
+                        <p>
+                            A website prototype of Resume Parser for NLP class. The website is currently nonfunctional and was only used to represent the final result. The final version is above this post.
+                        </p>
+                    </div>
+                </a>
+
+                <a href="machine-learning.html" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Machine Learning Project</h2>
+                        <p>A machine learning project for mid semester exam.</p>
+                    </div>
+                </a>
+
+                <a href="wise-words.php" target=""> 
+                    <div class="website-desc">
+                        <h2>Wise words</h2>
+                        <p>Tried to use php to generate random word from a set of array.</p>
+                    </div>
+                </a>
+
+                <a href="https://vitopm.github.io/A04/" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Covac.</h2>
+                        <p>A team based project for web programming bootcamp.</p>
+                    </div>
+                </a>
+
+                <a href="https://vitopm.github.io/Tea-Cozy/" target="_blank">
+                    <div class="website-desc">
+                        <h2>Tea Cozy</h2>
+                        <p>A website made for promoting a Tea Cafe. This one is not real, I made this from the assignment given by the online course I'm was enrolled.</p>
+                    </div>
+                </a>
+    
+                <a href="https://vitopm.github.io/excursion/" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Excursion</h2>
+                        <p>A promotional website for an upcoming release of excursion application. This one is also an imaginary application and was an assignment from my online course.</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </main> 
+
+    <footer>
+        <p>made with love <a href="https://youtu.be/Tr97MQiqW38" target="_blank">❤️</a> by <a href="/index.html">vitopm</a></p>
+    </footer>
+</body>
+</html>
