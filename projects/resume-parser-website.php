865fef4f3b9fab4bfd871a600e02bfe9079d8ff6
Vito <vito.pramudita1502@gmail.com>
2022-10-21 17:25:39 +0700

    Added new page for resume parser

---
+++ projects/resume-parser-website.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="resources/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="resources/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../resources/css/style.css">
+    <link rel="stylesheet" href="../resources/css/responsive.css">
+    <title>vito pm - resume parser</title>
+</head> 
+<body>
+    <header>
+        <p class="name-logo"><a href="../index.php">vitopm</a></p>
+        <div class="nav-bar">
+            <ul>
+                <li><a href="../about-me.php">about</a></li>
+                <li><a href="../contact.php">contact</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        <div class="intro">
+            <h1 class="intro--name">Resume Parser Website</h1>
+
+            <h3 class="intro--description">A NLP project.</h3>
+        </div>
+
+        <div class="content">
+            
+            <p>
+                In semester 4, we were given a project from Natural Language Processing (NLP) class and splitted into several group members. The group members were me, Cheryl Almeira, and Michelle Angela. 
+            </p>
+            <br>
+            <p>
+                There were several idea candidates, in the end we chose to make a web that capable of extracting information from a CV or resume. Before jumping to the backend programming, we had to make ourselves a dummy website of our idea, which we call Resume Parser.
+            </p>
+            <br>
+            <p>
+                Since we have to merge this project with Entrepreneurship class, 
+                we decided to create two features, these are business and personal feature. Business feature allow user to extract information from multiple resumes and add criteria to filter those resumes. Whilst the personal feature is limited to extract information of a resume at a time and the ability to score the resume by counting missing data within the uploaded resume.
+            </p>
+            <br>
+            <p>
+                The most challenging part of this project was which core feature that a business need. We had to research whether adding that would give a value that a business need. I asked several friends who worked in human resource development division, they said this was a good idea, since there weren't many product like this that could help them from reading tons of resume one by one.
+            </p>
+            <br>
+            <p>
+                Alternatively, you can click the link below to visit the projects.
+            </p>
+
+            <div class="project-website-list">
+                <a href="Resume-Parser" target="_blank"> 
+                    <div class="website-desc">
+                        <h2>Resume Parser Website</h2>
+                        <p>
+                            Click here to see the app.
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+            <p>
+                Here I attached some of the screenshots from the prototype.
+            </p> 
+            <br>
+            <p>
+                This is the homepage where user can choose to access the website as a business or as a personal user.
+            </p>
+            
+            <img class ="project-img" src="../resources/images/projects/resume-parser/homepage.jpg" alt="">
+            
+            <p>
+                And if we choose to enter the website as a business, we have the feature to upload several resumes.
+            </p>
+
+            <img class ="project-img" src="../resources/images/projects/resume-parser/business-upload.png" alt="">
+
+            <p>
+                The following feature allows user to enter a few criterions and then filter the extracted information to get the most suitable applicant.
+            </p>
+
+            <img class ="project-img" src="../resources/images/projects/resume-parser/business-criteria.png" alt="">
+
+            <p>
+                Since we havent worked on the backend process, so the result is going to be like this.
+            </p>
+
+            <img class ="project-img" src="../resources/images/projects/resume-parser/result.png" alt="">
+
+            <p>
+                That's all for the business features, now head on to the personal features. Since we haven't done the backend, the appearance from personal feature is going to be the same as the business, the main difference is the criteria page.
+            </p>
+
+            
+
+        </div>
+    </main> 
+
+    <footer>
+        <p>made with love <a href="https://youtu.be/Tr97MQiqW38" target="_blank">❤️</a> by <a href="../index.php">vitopm</a></p>
+    </footer>
+</body>
+</html>
